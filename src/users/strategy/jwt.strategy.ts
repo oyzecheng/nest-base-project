eadc71f18b2bedfba733aa8a68cc47dd689809d0
ouyangzecheng <ouyangzecheng@gmail.com>
2024-10-23 15:33:40 +0800

    fix: jwt.strategy.ts bug

--- src/users/strategy/jwt.strategy.ts
+++ src/users/strategy/jwt.strategy.ts
@@ -2,15 +2,18 @@ import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { ConfigService } from '@nestjs/config';
 import { PrismaService } from '../../prisma/prisma.service';
+import ConfigEnum from '../../enum/configEnum';
+import { Injectable } from '@nestjs/common';
 
+@Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor(
     config: ConfigService,
     private prisma: PrismaService,
   ) {
     super({
-      jstFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: config.get('KEY'),
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: config.get(ConfigEnum.JWT_SECRET),
     });
   }
 
